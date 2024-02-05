79e078a97cb3f40e9432c0edb788290ba49e13d2
Stanescu Victor <stanescuvictor12@gmail.com>
2024-02-05 12:02:11 +0100

    Set up and board created

---
+++ JavaChess/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+    JFrame window = new JFrame("Simple Chess");
+    //when we close the window we shut down the program
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //can not resize the window
+    window.setResizable(false);
+
+    GamePanel gp = new GamePanel();
+    window.add(gp);
+    window.pack();
+
+    //middle of the screen
+    window.setLocationRelativeTo(null);
+    //see it on the screen
+    window.setVisible(true);
+
+
+    gp.launchGame();
+
+    }
+}
